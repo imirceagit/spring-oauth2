868acd4b7adf3faddcd41c80c8c87067406233f5
Mircea Ionita <mionita@deloittece.com>
2018-09-10 19:22:08 +0300

    spring-oauth2 server/client/resource configuration

---
+++ client-server/src/main/java/com/mient/clientserver/HomeController.java
@@ -0,0 +1,24 @@
+package com.mient.clientserver;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @Autowired
+    private OAuth2RestTemplate restTemplate;
+
+    @Value("${message.api:http://localhost:18080/message}")
+    String messageApi;
+
+    @GetMapping("/")
+    public String home(Model model) {
+        String message = restTemplate.getForObject(messageApi, String.class);
+        return message;
+    }
+}
---
+++ resource-server/src/main/java/com/mient/resourceserver/ResourceController.java
@@ -0,0 +1,13 @@
+package com.mient.resourceserver;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ResourceController {
+
+    @GetMapping("/message")
+    public String randomMessage() {
+        return "Multa sanatate!";
+    }
+}
